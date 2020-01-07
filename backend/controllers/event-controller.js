c9609186df2d3f2299fa67afcadcfc998f836a2a
Ian Kimble <ian.kimble@eRT.com>
2020-01-06 21:48:43 -0500

    commit one

---
+++ backend/controllers/event-controller.js
@@ -0,0 +1,72 @@
+const Event = require("../models/Event-model");
+
+// Get all Events and send to frontend
+
+// ROUTE CRUD WILL BE MANAGED VIA POSTMAN UNTIL ADMIN INTERFACE AND LOGIC
+// IS BUILT
+//____________________________________________________________//
+
+// All events
+exports.allEvents = (req, res) => {
+  Event.find().exec((err, events) => {
+    if (err) {
+      return res.status(400).json({
+        err: "err"
+      });
+    }
+    res.json(events);
+  });
+};
+
+// All events w/ pagination
+exports.eventsPagination = (req, res) => {
+  const page = parseInt(req.query.page);
+  const limit = parseInt(req.query.limit);
+
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+
+  const result = {};
+
+  result.next = {
+    page: page + 1,
+    limit: limit
+  };
+  Event.find().exec((err, events) => {
+    if (err) {
+      return res.status(400).json({ err: "err" });
+    }
+    result.events = events.reverse().slice(startIndex, endIndex);
+    res.jon(result.events);
+  });
+};
+
+// Create an event ( will be created from postman)
+exports.addEvent = (req, res) => {
+  let newEvent = new Event(req.body);
+  newEvent.save((err, result) => {
+    if (err) {
+      return res.status(400).json({
+        err: "err"
+      });
+    }
+    res.status(200).json(result);
+    console.log("event successfully created.");
+  });
+};
+
+// Delete Event (will be removed from postman)
+exports.deleteEvent = (req, res) => {
+  let event = req.body;
+  event.remove((err, result) => {
+    if (err) {
+      return res.status(400).json({
+        err: "err"
+      });
+    }
+    res.json({ msg: "event removed successfully." });
+  });
+};
+
+// Update an Event (will be removed from postman)
+exports.updateEvent = (req, res) => {};
---
+++ greenprintGatsby/blog/gatsby-browser.js
@@ -0,0 +1,12 @@
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/browser-apis/
+ */
+
+// You can delete this file if you're not using it
+
+// -----------------------------------------------------------------//
+// have to use this string in order to import react bootsrap globally
+import "./node_modules/bootstrap/dist/css/bootstrap.css"
+//-----------------------------------------------------------------//
